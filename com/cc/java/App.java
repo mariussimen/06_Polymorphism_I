de870e21b8dc81a04aedba5c552e4fb23885d709
mariussimen <theodores03@yahoo.fr>
2024-07-19 12:08:48 +0200

    Overloading/19.07

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,11 +4,46 @@ public class App {
     
     public static void main(String[] args) {
      
-        
+        Queen queen = new Queen();
+        Worker worker = new Worker();
+        Drone drone = new Drone();
+
+        pollObj(queen);
+        pollObj(worker);
+        pollObj(drone);
+   
+    //   output(queen.doYourJob());
+    //   output(queen.fly()); 
+    //   output("------------------");
+    
+    //   output(worker.doYourJob());
+    //   output(worker.fly());  
+    //   output("-------------");
+
+    //   output(drone.doYourJob());
+    //   output(drone.fly());
+    //   output("--------------");
+
+    }
+//   Overloading 
+    private static void pollObj(Queen obj){
+        output(obj.doYourJob());
+        output(obj.fly());
+        output("----------------");
+    }
+    // Overloading
+    private static void pollObj(Worker obj){
+        output(obj.doYourJob());
+        output(obj.fly());
+        output("----------------");
+    }
+    // Overloading
+    private static void pollObj(Drone obj){
+        output(obj.doYourJob());
+        output(obj.fly());
+        output("----------------");
     }
 
-   
-   
     private static void output(String outputStr) {
         System.out.println(outputStr);
     }
---
+++ com/cc/java/Queen.java
@@ -0,0 +1,10 @@
+package com.cc.java;
+
+public class Queen extends HoneyBee {
+
+
+    public String doYourJob(){
+        return "I'm a Queen, any questions?";
+    }
+
+}
